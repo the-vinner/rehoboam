4a145d268106afbd30e53aaef82672a335c2af63
vince roy <vince@potionapps.com>
2021-12-26 21:31:31 -0500

    load schemas in content sidebar

--- frontend/admin/src/components/SidebarSchemas/SidebarSchemas.tsx
+++ frontend/admin/src/components/SidebarSchemas/SidebarSchemas.tsx
@@ -1,11 +1,21 @@
-import { faPlus } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@potionapps/utils";
-import BtnSmallBordered from "components/Btn/BtnSmallBordered";
-import { routeNames } from "root/routes/routeNames";
 import { defineComponent } from "vue";
+import { faEdit, faPencilAlt, faPlus } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@potionapps/utils";
+import { RootQueryTypeSchemaCollectionArgs } from "shared/types";
+import { routeNames } from "root/routes/routeNames";
+import BtnSmallBordered from "components/Btn/BtnSmallBordered";
+import schemaCollection from "shared/models/Schemas/Schema/schemaCollection.gql";
+import useQueryTyped from "hooks/useQueryTyped";
 
 export default defineComponent({
   setup() {
+    const variables: RootQueryTypeSchemaCollectionArgs = {
+      first: 200,
+    };
+    const { data } = useQueryTyped({
+      query: schemaCollection,
+      variables,
+    });
     return () => {
       return (
         <div>
@@ -16,6 +26,38 @@ export default defineComponent({
             New Content Type
             <FontAwesomeIcon icon={faPlus} />
           </BtnSmallBordered>
+          <div class="flex flex-col gap-1 mt-3">
+            {data.value?.schemaCollection?.edges?.map((edge) => {
+              if (edge?.node) {
+                return (
+                  <button
+                    class={[
+                      "capitalize",
+                      "flex",
+                      "group",
+                      "items-center",
+                      "gap-2",
+                      "hover:text-slate-900",
+                      "text-slate-600",
+                      "py-1.5",
+                      "text-sm",
+                    ]}
+                  >
+                    <div>{edge.node.title}</div>
+                    <router-link
+                      class={[
+                        "group-hover:opacity-100",
+                        "opacity-0"
+                      ]}
+                      to={{ name: routeNames.schema, params: { id: edge.node?.internalId } }}
+                    >
+                      <FontAwesomeIcon icon={faPencilAlt} />
+                    </router-link>
+                  </button>
+                );
+              }
+            })}
+          </div>
         </div>
       );
     };
