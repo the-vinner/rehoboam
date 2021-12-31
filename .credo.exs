b7fc701fa500e440d3eeb4cb2af20500609851b0
vince roy <vince@potionapps.com>
2021-12-31 12:41:09 -0500

    add credo fie

---
+++ .credo.exs
@@ -0,0 +1,17 @@
+%{
+  configs: [
+    %{
+      name: "default",
+      files: %{
+        included: ["lib/", "src/", "web/", "apps/"],
+        excluded: []
+      },
+      plugins: [],
+      requires: [],
+      strict: false,
+      parse_timeout: 5000,
+      color: true,
+      checks: []
+    }
+  ]
+}
